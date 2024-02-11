10709d89e7db260f5899f8a19db83e21f2a37ad9
abhi07nav <112764910+abhi07nav@users.noreply.github.com>
2024-02-11 12:36:43 +0530

    student classy

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public Student(){
+        System.out.println("student object");
+        //System.out.println("Dimag Kharab Ho gya 2 din se yehi aatka pada hoon");
+    }
+    public void fuckyar(){
+        System.out.println("Fuck jenkins");    
+    }
+    
+}
--- main.java
+++ main.java
@@ -2,5 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Dimag Kharab Ho gya 2 din se yehi aatka pada hoon");
+        Student student=new Student();
+        student.fuckyar();
     }
 }
